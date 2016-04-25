faba5093336ae0c56352b160a61e601da391b772
HexUni <HexUni@users.noreply.github.com>
2016-04-25 13:25:35 +0200

    Create boot.cpp

---
+++ prog/boot.cpp
@@ -0,0 +1,20 @@
+
+#include <windows.h>
+#include <conio.h>
+
+#include <stdio.h>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+  system ("start E:\\PROGRAMS\\PeerBlockPortable\\PeerBlockPortable.exe"); //for opening stackoverflow through google chrome , if chorme.exe is in that folder..
+  system ("start I:\\Portable\\ThunderbirdPortable\\ThunderbirdPortable.exe"); //for opening stackoverflow through google chrome , if chorme.exe is in that folder..
+
+  ///system ("start E:\\PROGRAMS\\MicroSIPPortable\\MicroSIPPortable.exe"); //for opening stackoverflow through google chrome , if chorme.exe is in that folder..
+  ///int result = system("E:\\PROGRAMS\\PeerBlockPortable\\PeerBlockPortable.exe");
+
+  return 0;
+  getch();
+}
